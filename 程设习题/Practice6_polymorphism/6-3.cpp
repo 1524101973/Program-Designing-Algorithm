05ca2bb4e2cd76937a15ab1977c24b39c1dc31e7
Hojin <63623022+1524101973@users.noreply.github.com>
2022-04-26 15:05:53 +0900

    Add files via upload

---
+++ 程设习题/Practice6_polymorphism/6-3.cpp
@@ -0,0 +1,57 @@
+/*
+C:这是什么鬼delete
+查看提交统计提问
+总时间限制: 1000ms 内存限制: 65536kB
+描述
+程序填空输出指定结果
+
+#include <iostream> 
+using namespace std;
+class A 
+{ 
+public:
+	A() { }
+// 在此处补充你的代码
+}; 
+class B:public A { 
+	public: 
+	~B() { cout << "destructor B" << endl; } 
+}; 
+int main() 
+{ 
+	A * pa; 
+	pa = new B; 
+	delete pa; 
+	return 0;
+}
+输入
+无
+输出
+destructor B
+destructor A
+*/
+
+#include <iostream> 
+using namespace std;
+class A 
+{ 
+public:
+	A() { }
+// 在此处补充你的代码
+    virtual ~A()
+    {
+        cout << "destructor A" << endl;
+    }
+}; 
+class B:public A { 
+	public: 
+	~B() { cout << "destructor B" << endl; } 
+}; 
+int main() 
+{ 
+	A * pa; 
+	pa = new B; 
+	delete pa; 
+    system("pause");
+	return 0;
+}
